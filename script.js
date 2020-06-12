965b492755d9cbf1ff256333e943d767db02c1eb
Deepak Bhamla <dbhamla7@gmail.com>
2020-06-12 15:20:00 +0530

    sticky header

--- script.js
+++ script.js
@@ -3,6 +3,29 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
 
+
+(function ($) {
+    $(document).ready(function () {
+
+        // hide .navbar first
+        $(".navbar").hide();
+
+        // fade in .navbar
+        $(function () {
+            $(window).scroll(function () {
+
+                // set distance user needs to scroll before we start fadeIn
+                if ($(this).scrollTop() > 550) {
+                    $('.navbar').fadeIn();
+                } else {
+                    $('.navbar').fadeOut();
+                }
+            });
+        });
+
+    });
+}(jQuery));
+
 // navbar fixed at top
 window.onscroll = function () { myFunction() };
 var navbar = document.getElementById("navbar");
@@ -15,4 +38,3 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
-
